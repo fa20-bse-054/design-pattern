5487560bcff6f354c41e8eeb10ad47f02759407f
fa20-bse-054 <101318480+fa20-bse-054@users.noreply.github.com>
2023-10-20 08:57:57 +0500

    Visitor_pattern_new_example

---
+++ Visitor_newExample/src/add_computerpart/MultimediaVisitor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package add_computerpart;
+
+/**
+ *
+ * @author fa20-bse-054
+ */
+public class MultimediaVisitor implements ComputerPartVisitor {
+    @Override
+    public void visit(Motherboard motherboard) {
+        System.out.println("Adding multimedia support to the motherboard.");
+    }
+}
