f684af038b3c9d65823d9c9baf40a008f58e7ce3
fa20-bse-054 <101318480+fa20-bse-054@users.noreply.github.com>
2023-11-02 12:07:00 +0500

    midterm_decorator

---
+++ midterm 23 decorator/mid_decorator/src/mid_decorator/Food.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mid_decorator;
+
+/**
+ *
+ * @author fa20-bse-054
+ */
+interface Food {
+    double getCost();
+    String getDescription();
+}
---
+++ midterm 23 decorator/mid_decorator/src/mid_decorator/Roast.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mid_decorator;
+
+/**
+ *
+ * @author fa20-bse-054
+ */
+public class Roast {
+    
+}
